fb49f56344ee593647555cab98960099bdcca411
glomvardos <gerapes90@yahoo.gr>
2021-07-07 21:06:16 +0300

    Random question

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,9 +1,18 @@
-const Question = ({ city }) => {
-  return (
-    <h2 className='max-w-sm sm:w-80 font-bold text-2xl my-5 text-blueTitle'>
-      City is the capital of
-    </h2>
-  )
+const Question = ({ randomNum, city, flag }) => {
+  if (randomNum === 0) {
+    return (
+      <h2 className='max-w-sm sm:w-80 font-bold text-2xl my-5 text-blueTitle'>
+        City is the capital of
+      </h2>
+    )
+  }
+  if (randomNum === 1) {
+    return (
+      <h2 className='max-w-sm sm:w-80 font-bold text-2xl my-5 text-blueTitle'>
+        Which country does this flag belong to?
+      </h2>
+    )
+  }
 }
 
 export default Question
