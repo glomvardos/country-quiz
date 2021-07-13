200c2c0c9cbf4b7c71f0e17b784347bd7f3bbed1
glomvardos <gerapes90@yahoo.gr>
2021-07-13 18:55:31 +0300

    code refactoring for countries-actions.js

--- src/store/counties-actions.js
+++ src/store/counties-actions.js
@@ -3,22 +3,25 @@ import { loadingActions } from './loading-slice'
 
 export const fetchCountries = () => {
   return async dispatch => {
-    try {
-      const responseEu = await fetch('https://restcountries.eu/rest/v2/region/europe')
-      const responseAmerica = await fetch('https://restcountries.eu/rest/v2/region/americas')
+    const urls = [
+      'https://restcountries.eu/rest/v2/region/europe',
+      'https://restcountries.eu/rest/v2/region/americas',
+    ]
 
-      if (!responseEu.ok || !responseAmerica.ok) {
-        throw new Error('Something went wrong.')
-      }
-      const dataEu = await responseEu.json()
-      const dataAmerica = await responseAmerica.json()
+    const [countriesEu, countriesAmerica] = await Promise.all(
+      urls.map(url =>
+        fetch(url)
+          .then(res => {
+            if (!res.ok) throw new Error('Something went wrong!')
+            return res.json()
+          })
+          .catch(err => dispatch(loadingActions.setIsNotLoading()))
+      )
+    )
 
-      const data = [...dataEu, ...dataAmerica]
+    const data = [...countriesEu, ...countriesAmerica]
 
-      dispatch(countriesActions.getCountries(data))
-      dispatch(loadingActions.setIsNotLoading())
-    } catch (err) {
-      dispatch(loadingActions.setIsNotLoading())
-    }
+    dispatch(countriesActions.getCountries(data))
+    dispatch(loadingActions.setIsNotLoading())
   }
 }
